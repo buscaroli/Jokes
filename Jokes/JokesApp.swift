a21c8dabf36419f925b96f0f8c6c30ed25dafcb8
mabus <m.buscaroli@outlook.com>
2021-06-15 17:27:53 +0100

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Matteo on 15/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
